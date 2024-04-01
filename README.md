29716dcc9b267c2c11b849fe27d00d0d3153a56d
Vicky <vicky9060993097@gmail.com>
2024-04-01 17:53:46 +0530

    Updated readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# Personal Portfolio
+
+Welcome to my personal portfolio repository! This repository contains the source code for my personal portfolio website.
+
+## Introduction
+
+This repository hosts the code for my personal portfolio website, which is built using React. It showcases my projects, skills, experiences, and education. Feel free to explore the codebase and provide any feedback or suggestions.
+
+## Features
+
+- **Responsive Design**: The portfolio website is designed to be responsive, ensuring optimal viewing experience across various devices.
+- **Sections**: The website is divided into several sections including home, education, experience, skills, and projects, allowing visitors to easily navigate and explore.
+- **Dynamic Content**: Content in each section is dynamically generated using React components, making it easy to update and maintain.
+- **Cool Animations**: The website features engaging animations to enhance user experience and add visual appeal.
+
+# Running the Personal Portfolio Locally
+
+To run this project locally, follow these steps:
+
+1. Clone this repository to your local machine using:
+   ```bash
+   git clone https://github.com/Vickykumar1001/Personal-Portfolio.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd Personal-Portfolio
+   ```
+3. Install dependencies using npm:
+   ```bash
+   npm install
+   ```
+4. Start the development server:
+   ```bash
+   npm start
+   Usage
+   Feel free to use this project as a template for your own personal portfolio website. Customize it to your liking by modifying the React components, styling, and content. Add your own projects, skills, experiences, and education to showcase your abilities effectively.
+   ```
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-import {ParticlesBg} from "./ParticlesBg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import { ParticlesBg } from "./ParticlesBg";
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Student", "Web Developer", "Programmer"];
+  const toRotate = ["Student", "Web Developer", "Programmer"];
   const period = 2000;
 
   useEffect(() => {
@@ -19,63 +19,100 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
-    
-    <section className="banner"  id="home">
-      <Container>     
-      <ParticlesBg />
-      <Row className="aligh-items-center">
+    <section className="banner" id="home">
+      <Container>
+        <ParticlesBg />
+        <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__bounceInLeft" : ""}>
-              
-                <span className={isVisible ? "tagline animate__animated animate__bounce animate__slow" : "tagline"}>Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Vicky`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><br/> <span className="wrap">{text}</span></span></h1>
-                  <p>I am a student at NIT Mizoram pursuing Electronics and Communication Engineering. Apart from my coursework, I have actively pursued my interest in computer science through various online courses and projects.</p>
-                  <a href="mailto:vickykum1001@gmail.com"><button>Let’s Connect <ArrowRightCircle size={25} /></button></a>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__bounceInLeft" : ""
+                  }
+                >
+                  <span
+                    className={
+                      isVisible
+                        ? "tagline animate__animated animate__bounce animate__slow"
+                        : "tagline"
+                    }
+                  >
+                    Welcome to my Portfolio
+                  </span>
+                  <h1>
+                    {`Hi! I'm Vicky`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                    >
+                      <br /> <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    I am a student at NIT Mizoram pursuing Electronics and
+                    Communication Engineering. Apart from my coursework, I have
+                    actively pursued my interest in computer science through
+                    various online courses and projects.
+                  </p>
+                  <a href="mailto:vickykum1001@gmail.com">
+                    <button>
+                      Let’s Connect <ArrowRightCircle size={25} />
+                    </button>
+                  </a>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-    
-  )
-}
+  );
+};
